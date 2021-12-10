a1c4813e7305e8d8354792f6c5e47adac3f59b98
Sachin Tripathi <tripathisachinn@gmail.com>
2021-12-10 23:41:20 +0530

    Updates made

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
 import Header from './components/partials/Header';
 import Footer1 from './components/partials/Footer1';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter >
       <Header />
-      <Home />
+      <Routes>
+        <Route path = "/" element={<Home />} />
+        <Route path = "/about" element={<About />} />
+        <Route path = "/contact" element={<Contact />} />
+      </Routes>
       <Footer1 />
-      
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/partials/Header.js
+++ src/components/partials/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {BrowserRouter as Router, } from "react-router-dom";
-  import { Navbar,Nav,NavDropdown,Form,FormControl,Button } from 'react-bootstrap'
+import { Navbar,Nav,NavDropdown,Form,FormControl, } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
 
 //   Navbar working on page
 class Header extends React.Component{
@@ -10,15 +10,15 @@ class Header extends React.Component{
             <div>
                 <div className="row">
                     <div className="col-md-12">
-                        <Router>
+                        
                             <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-                                <Navbar.Brand href="#home">React Bootstrap Navbar</Navbar.Brand>
+                                <Navbar.Brand href="#home">Movie Theatre</Navbar.Brand>
                                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                                 <Navbar.Collapse id="basic-navbar-nav">
                                     <Nav className="mr-auto">
-                                    <Nav.Link href="/">Home</Nav.Link>
-                                    <Nav.Link href="/about-us">Contact Us</Nav.Link>
-                                    <Nav.Link href="/contact-us">About Us</Nav.Link>
+                                    <Link to="/">Home</Link>
+                                    <Link to="/about">Contact Us</Link>
+                                    <Link to="/contact">About Us</Link>
                                     <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -29,12 +29,12 @@ class Header extends React.Component{
                                     </Nav>
                                     <Form inline>
                                     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                                    <Button variant="outline-success">Search</Button>
+                                    {/* <Button variant="outline-success">Search</Button> */}
                                     </Form>
                                 </Navbar.Collapse>
                             </Navbar>
                             <br />
-                        </Router>
+                        
                     </div>
                 </div>
             </div>
